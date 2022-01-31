67f5a0254889fe2e7465ce6670ba009d039d6381
SIddharth Tiwari <43677324+SIDtwr@users.noreply.github.com>
2022-01-31 23:05:51 +0000

    Update main.r
    Add code to repository

--- src/main.r
+++ src/main.r
@@ -59,28 +59,33 @@ covid_data %>%
 # Order 1
 poly_reg1  <- lm(formula = CASES~INDEX,
                 data = covid_data)
+summary(poly_reg1)
 # Order 2
 poly_reg2  <- lm(formula = CASES~INDEX + I(INDEX^2), 
                 data = covid_data)
+summary(poly_reg2)
 # Order 3
 poly_reg3 <- lm(formula = CASES~INDEX + I(INDEX^2) + I(INDEX^3), 
                 data = covid_data)
+summary(poly_reg3)
 # Order 4
 poly_reg4 <- lm(formula = CASES~INDEX + I(INDEX^2) + I(INDEX^3) + I(INDEX^4), 
                 data = covid_data)
+summary(poly_reg4)
 
 #log_model_one <- dynlm(log(CASES) ~ log(DATE), data = covid_data)
 
 
 ggplot(covid_data) +
-  geom_point(aes(INDEX, CASES, col = "Original")) +
+  geom_point(aes(INDEX, CASES, col = "Original-points")) +
+  geom_line(aes(INDEX, CASES, col = "Original-line")) +
   stat_smooth(method = "lm", formula = y~poly(x,1), aes(INDEX, poly_reg1$fitted.values, col = "Order 1")) +
   stat_smooth(method = "lm", formula = y~poly(x,2), aes(INDEX, poly_reg2$fitted.values, col = "Order 2")) +
   stat_smooth(method = "lm", formula = y~poly(x,3), aes(INDEX, poly_reg3$fitted.values, col = "Order 3")) +
   stat_smooth(method = "lm", formula = y~poly(x,4), aes(INDEX, poly_reg4$fitted.values, col = "Order 4")) +
   scale_colour_manual("",
-                      breaks = c("Original","Order 1","Order 2","Order 3","Order 4"),
-                      values = c("red","cyan","blue","orange","green")) +
+                      breaks = c("Original-points", "Original-line", "Order 1","Order 2","Order 3","Order 4"),
+                      values = c("black","black","cyan","blue","orange","green")) +
   theme_bw()
 
 
@@ -98,29 +103,33 @@ predict(poly_reg4, newdata = new.data)
 # Order 1
 poly_reg1_log  <- lm(formula = CASES~log(INDEX),
                  data = covid_data)
+summary(poly_reg1_log)
 # Order 2
 poly_reg2_log  <- lm(formula = CASES~log(INDEX) + I(INDEX^2), 
                  data = covid_data)
+summary(poly_reg2_log)
 # Order 3
 poly_reg3_log <- lm(formula = CASES~log(INDEX) + I(log(INDEX)^2) + I(log(INDEX)^3), 
                 data = covid_data)
+summary(poly_reg3_log)
 # Order 4
 poly_reg4_log <- lm(formula = CASES~log(INDEX) + I(log(INDEX)^2) + I(log(INDEX)^3) + I(log(INDEX)^4), 
                 data = covid_data)
+summary(poly_reg4_log)
 
 ggplot(covid_data) +
-  geom_point(aes(INDEX, CASES, col = "Original")) +
+  geom_point(aes(INDEX, CASES, col = "Original-points")) +
+  geom_line(aes(INDEX, CASES, col = "Original-line")) +
   stat_smooth(method = "lm", formula = y~poly(log(x),1), aes(INDEX, poly_reg1_log$fitted.values, col = "Order 1")) +
   stat_smooth(method = "lm", formula = y~poly(log(x),2), aes(INDEX, poly_reg2_log$fitted.values, col = "Order 2")) +
   stat_smooth(method = "lm", formula = y~poly(log(x),3), aes(INDEX, poly_reg3_log$fitted.values, col = "Order 3")) +
   stat_smooth(method = "lm", formula = y~poly(log(x),4), aes(INDEX, poly_reg4_log$fitted.values, col = "Order 4")) +
   scale_colour_manual("",
-                      breaks = c("Original","Order 1","Order 2","Order 3","Order 4"),
-                      values = c("red","cyan","blue","orange","green")) +
+                      breaks = c("Original-points", "Original-line","Order 1","Order 2","Order 3", "Order 4"),
+                      values = c("black","black","cyan","blue","orange","green")) +
   theme_bw()
 
 new.data <- data.frame(
   INDEX = c(1, 30, 60, 90, 120)
 )
 predict(poly_reg4_log, newdata = new.data)
-
