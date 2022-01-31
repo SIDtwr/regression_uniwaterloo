6ca4420dab33dd209fc41401654530506b5db4ff
SIddharth Tiwari <43677324+SIDtwr@users.noreply.github.com>
2022-01-31 22:39:29 +0000

    Update main.r
    Answer to Ques 1 & 2 of the assignment

--- src/main.r
+++ src/main.r
@@ -3,54 +3,124 @@
 #install.packages("devtools")
 #install.packages("tidyverse")
 #install.packages("caret")
-install.packages("ggplot2")
+#install.packages("ggplot2")
 
 #library(tidyverse)
 #library(caret)
 #theme_set(theme_classic())
 
 library("ggplot2")
+library("dplyr")
+library("magrittr")
+library("tidyverse")
+library("dynlm")
+
 
 # Download csv file
-download.file("https://raw.githubusercontent.com/SIDtwr/regression_uniwaterloo/main/data/covid_data.csv",destfile="/tmp/covid_data.csv",method="libcurl")
+#download.file("https://raw.githubusercontent.com/SIDtwr/regression_uniwaterloo/main/data/covid_data.csv",destfile="/tmp/covid_data.csv",method="libcurl")
 
-data <- read.csv("/tmp/covid_data.csv")
-print(data)
+covid_data <- read.csv("C:/Users/GZDX20/Documents/uniwaterloo/regression_uniwaterloo-main/data/covid_data.csv", header = TRUE)
+covid_data <- tibble::rowid_to_column(covid_data, "index")
 
-## Visulize Dataset
-ggplot(data) + 
-  geom_point(data(Date, Cases),size=3) + 
+####################### FORMAT DATAFRAME #############################
+
+# get rid of unecessary columns 
+covid_data <- covid_data[-c(4:5)]
+
+# rename columns
+colnames(covid_data) <- c('INDEX','DATE','CASES')
+
+# format Column DATE
+covid_data <- covid_data %>%
+  mutate(DATE = as.Date(DATE, format = "%m/%d/%Y"))
+
+# format column CASES
+covid_data <- covid_data %>%
+  mutate(CASES = as.numeric(gsub(",", "", CASES)))
+
+print(covid_data)
+summary(covid_data)
+
+####################### FORMAT DATAFRAME ##############################
+
+
+
+# plot the data using ggplot2 and pipes
+covid_data %>%
+  ggplot(aes(x = DATE, y = CASES)) +
+  geom_point(color = "darkorchid4") +
+  labs(title = "OMICRON COVID-19 Cases",
+       y = "Num of COVID Cases",
+       x = "Date") + theme_bw(base_size = 15)
+
+
+############ QUESTION ONE ###################
+
+# Order 1
+poly_reg1  <- lm(formula = CASES~INDEX,
+                data = covid_data)
+# Order 2
+poly_reg2  <- lm(formula = CASES~INDEX + I(INDEX^2), 
+                data = covid_data)
+# Order 3
+poly_reg3 <- lm(formula = CASES~INDEX + I(INDEX^2) + I(INDEX^3), 
+                data = covid_data)
+# Order 4
+poly_reg4 <- lm(formula = CASES~INDEX + I(INDEX^2) + I(INDEX^3) + I(INDEX^4), 
+                data = covid_data)
+
+#log_model_one <- dynlm(log(CASES) ~ log(DATE), data = covid_data)
+
+
+ggplot(covid_data) +
+  geom_point(aes(INDEX, CASES, col = "Original")) +
+  stat_smooth(method = "lm", formula = y~poly(x,1), aes(INDEX, poly_reg1$fitted.values, col = "Order 1")) +
+  stat_smooth(method = "lm", formula = y~poly(x,2), aes(INDEX, poly_reg2$fitted.values, col = "Order 2")) +
+  stat_smooth(method = "lm", formula = y~poly(x,3), aes(INDEX, poly_reg3$fitted.values, col = "Order 3")) +
+  stat_smooth(method = "lm", formula = y~poly(x,4), aes(INDEX, poly_reg4$fitted.values, col = "Order 4")) +
+  scale_colour_manual("",
+                      breaks = c("Original","Order 1","Order 2","Order 3","Order 4"),
+                      values = c("red","cyan","blue","orange","green")) +
   theme_bw()
 
-## Prepare training data & splitting into training and testing dataset
 
-# Set a seed value for reproducible results
-set.seed(70)
-# Split the data
-ind <- sample(x = nrow(data), size = floor(0.75 * nrow(data))
-# Store the value in train and test dataframes
-train <- data[ind,]
-test <- data[-ind,]
-
-# Generate the model
-model_one <- dynlm(Cases ~ Date, data = train)
-model_two <- dynlm(Cases ~ Date + I(Date^2), data = train)
-model_three <- dynlm(Cases ~ Date + I(Date^2) + I(Date^3), data = train)
-model_four <- dynlm(Cases ~ Date + I(Date^2) + I(Date^3) + I(Date^4), data = train)
-
-# Website links
-# https://www.pluralsight.com/guides/polynomial-functions-analysis-with-r
-# http://www.sthda.com/english/articles/40-regression-analysis/162-nonlinear-regression-essentials-in-r-polynomial-and-spline-regression-models/
-
-## Test 
-# Load the data
-#data("Boston", package = "MASS")
-# Split the data into training and test set
-#set.seed(123)
-#training.samples <- Boston$medv %>%
-  #createDataPartition(p = 0.8, list = FALSE)
-#train.data  <- Boston[training.samples, ]
-#test.data <- Boston[-training.samples, ]
+new.data <- data.frame(
+  INDEX = c(1, 30, 60, 90, 120)
+)
+predict(poly_reg4, newdata = new.data)
 
 
+
+
+############ QUESTION TWO ###################
+
+
+# Order 1
+poly_reg1_log  <- lm(formula = CASES~log(INDEX),
+                 data = covid_data)
+# Order 2
+poly_reg2_log  <- lm(formula = CASES~log(INDEX) + I(INDEX^2), 
+                 data = covid_data)
+# Order 3
+poly_reg3_log <- lm(formula = CASES~log(INDEX) + I(log(INDEX)^2) + I(log(INDEX)^3), 
+                data = covid_data)
+# Order 4
+poly_reg4_log <- lm(formula = CASES~log(INDEX) + I(log(INDEX)^2) + I(log(INDEX)^3) + I(log(INDEX)^4), 
+                data = covid_data)
+
+ggplot(covid_data) +
+  geom_point(aes(INDEX, CASES, col = "Original")) +
+  stat_smooth(method = "lm", formula = y~poly(log(x),1), aes(INDEX, poly_reg1_log$fitted.values, col = "Order 1")) +
+  stat_smooth(method = "lm", formula = y~poly(log(x),2), aes(INDEX, poly_reg2_log$fitted.values, col = "Order 2")) +
+  stat_smooth(method = "lm", formula = y~poly(log(x),3), aes(INDEX, poly_reg3_log$fitted.values, col = "Order 3")) +
+  stat_smooth(method = "lm", formula = y~poly(log(x),4), aes(INDEX, poly_reg4_log$fitted.values, col = "Order 4")) +
+  scale_colour_manual("",
+                      breaks = c("Original","Order 1","Order 2","Order 3","Order 4"),
+                      values = c("red","cyan","blue","orange","green")) +
+  theme_bw()
+
+new.data <- data.frame(
+  INDEX = c(1, 30, 60, 90, 120)
+)
+predict(poly_reg4_log, newdata = new.data)
 
